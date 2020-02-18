2324a6a800199dc868578cba6193ea2dca77e93a
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-02-18 17:35:12 +0100

    Add labels

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-const YAML = require('yaml');
-const fs = require('fs');
+const YAML = require("yaml");
+const fs = require("fs");
+const path = require("path");
 
 const yamlFileLocation = process.argv[2];
+const translationsFolder = process.argv[3];
 
-const file = fs.readFileSync(yamlFileLocation, 'utf8');
+const file = fs.readFileSync(yamlFileLocation, "utf8");
 const yamlMeasures = YAML.parse(file).measures_config;
+
+const translations = fs
+  .readdirSync(translationsFolder)
+  .reduce((result, translationFilePath) => {
+    const translationFile = fs.readFileSync(
+      translationsFolder + translationFilePath,
+      "utf8"
+    );
+    const yamlTranslations = YAML.parse(translationFile).pim_measure;
+    const locale = path
+      .basename(translationFilePath, ".yml")
+      .replace("jsmessages.", "");
+
+    if (0 !== translationFilePath.indexOf("jsmessages")) return result;
+
+    return { ...result, [locale]: yamlTranslations };
+  }, {});
 
 const lines = Object.keys(yamlMeasures).map(value => {
   const measureFamilyName = value;
+  const labels = Object.keys(translations).reduce((result, locale) => {
+    if (
+      undefined === translations[locale] ||
+      undefined === translations[locale].families ||
+      undefined === translations[locale].families[measureFamilyName]
+    )
+      return result;
+
+    return {
+      ...result,
+      [locale]: translations[locale].families[measureFamilyName]
+    };
+  }, {});
   const standardUnit = yamlMeasures[value].standard;
   const units = Object.keys(yamlMeasures[value].units).map(unitCode => ({
     code: unitCode,
@@ -19,11 +51,13 @@ const lines = Object.keys(yamlMeasures).map(value => {
     symbol: yamlMeasures[value].units[unitCode].symbol
   }));
 
-  return `('${measureFamilyName}', '${standardUnit}', '${JSON.stringify(units)}')`;
+  return `('${measureFamilyName}', '${standardUnit}', '${JSON.stringify(
+    labels
+  )}', '${JSON.stringify(units)}')`;
 });
 
 console.log(`
-INSERT INTO \`akeneo_measurement\` (\`code\`, \`standard_unit\`, \`units\`)
+INSERT INTO \`akeneo_measurement\` (\`code\`, \`standard_unit\`, \`labels\`, \`units\`)
 VALUES
-    ${lines.join(',\n    ')};
+    ${lines.join(",\n    ")};
 `);
