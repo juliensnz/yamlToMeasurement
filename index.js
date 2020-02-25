bb384c61a9f9cfce13b0c6d1f7d292863c816e50
Valentin Dijkstra <valentindijkstra@gmail.com>
2020-02-25 13:09:39 +0100

    Translations folder is now optional, merge labels from yml file

--- index.js
+++ index.js
@@ -8,22 +8,23 @@ const translationsFolder = process.argv[3];
 const file = fs.readFileSync(yamlFileLocation, "utf8");
 const yamlMeasures = YAML.parse(file).measures_config;
 
-const translations = fs
-  .readdirSync(translationsFolder)
-  .reduce((result, translationFilePath) => {
-    const translationFile = fs.readFileSync(
-      translationsFolder + translationFilePath,
-      "utf8"
-    );
-    const yamlTranslations = YAML.parse(translationFile).pim_measure;
-    const locale = path
-      .basename(translationFilePath, ".yml")
-      .replace("jsmessages.", "");
+const translations = translationsFolder
+  ? fs.readdirSync(translationsFolder).reduce((result, translationFilePath) => {
+      console.log(translationFilePath);
+      const translationFile = fs.readFileSync(
+        translationsFolder + translationFilePath,
+        "utf8"
+      );
+      const yamlTranslations = YAML.parse(translationFile).pim_measure;
+      const locale = path
+        .basename(translationFilePath, ".yml")
+        .replace("jsmessages.", "");
 
-    if (0 !== translationFilePath.indexOf("jsmessages")) return result;
+      if (0 !== translationFilePath.indexOf("jsmessages")) return result;
 
-    return { ...result, [locale]: yamlTranslations };
-  }, {});
+      return { ...result, [locale]: yamlTranslations };
+    }, {})
+  : {};
 
 const getLabels = (translations, type, key) =>
   Object.keys(translations).reduce((result, locale) => {
@@ -41,18 +42,28 @@ const getLabels = (translations, type, key) =>
   }, {});
 
 const lines = Object.keys(yamlMeasures).map(measureFamilyName => {
-  const labels = getLabels(translations, "families", measureFamilyName);
+  const labels = {
+    ...getLabels(translations, "families", measureFamilyName),
+    ...yamlMeasures[measureFamilyName].labels
+  };
   const standardUnit = yamlMeasures[measureFamilyName].standard;
-  const units = Object.keys(yamlMeasures[measureFamilyName].units).map(unitCode => ({
-    code: unitCode,
-    labels: getLabels(translations, "units", unitCode),
-    convert: yamlMeasures[measureFamilyName].units[unitCode].convert.map(convertObject => {
-      const operator = Object.keys(convertObject)[0];
+  const units = Object.keys(yamlMeasures[measureFamilyName].units).map(
+    unitCode => ({
+      code: unitCode,
+      labels: {
+        ...getLabels(translations, "units", unitCode),
+        ...yamlMeasures[measureFamilyName].units[unitCode].labels
+      },
+      convert_from_standard: yamlMeasures[measureFamilyName].units[unitCode].convert.map(
+        convertObject => {
+          const operator = Object.keys(convertObject)[0];
 
-      return { operator, value: convertObject[operator] };
-    }),
-    symbol: yamlMeasures[measureFamilyName].units[unitCode].symbol
-  }));
+          return { operator, value: convertObject[operator] };
+        }
+      ),
+      symbol: yamlMeasures[measureFamilyName].units[unitCode].symbol
+    })
+  );
 
   return `('${measureFamilyName}', '${JSON.stringify(
     labels
